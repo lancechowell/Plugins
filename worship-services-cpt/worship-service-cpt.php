1e028a9ae6b6c53b846e399e5a2934b63bad0e55
Lance Howell <lance@lancehowell.com>
2020-04-19 12:59:46 -0500

    initilizing repository

---
+++ worship-services-cpt/worship-service-cpt.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Worship Services CPT
+Author: Lance Howell
+Description: This plugin will set-up the custom post types for Worship Services for 
+             a church service to include videos and notes along with the videos. 
+Version: 1.0
+             */
+
+

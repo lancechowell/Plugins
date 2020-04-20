bbb6d62a6e59a65459f4a91397185bf418e4165c
Lance Howell <lance@lancehowell.com>
2020-04-20 07:18:27 -0500

    added acf

--- worship-services-cpt/worship-service-cpt.php
+++ worship-services-cpt/worship-service-cpt.php
@@ -1,11 +1,12 @@
 <?php
 /*
 Plugin Name: Worship Services CPT
-Author: Lance Howell
 Description: This plugin will set-up the custom post types for Worship Services for 
-             a church service to include videos and notes along with the videos. 
+a church service to include videos and notes along with the videos. 
+Author: Lance Howell
+Author URI: "https://www.lancehowell.com/contact-us/
 Version: 1.0
-             */
+*/
 
             function cptui_register_my_cpts_worship_service() {
 
